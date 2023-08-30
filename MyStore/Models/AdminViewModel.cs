67c7fb5ad4e7710e4dd787460ff0792d6cf564a8
jawad0873 <jawadaurangzeb0873@gmail.com>
2023-08-30 10:05:52 -0700

    admin controller and viewmodel added

---
+++ MyStore/Models/AdminViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MyStore.Models
+{
+    public class AdminViewModel
+    {
+        public int Id { get; set; }  // You can use an ID field if needed
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<string> Roles { get; set; } = new List<string>();
+
+    }
+}
