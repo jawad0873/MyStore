f2fdfc0e96ec7900ae8b49da225d0015118a5e46
jawad0873 <jawadaurangzeb0873@gmail.com>
2023-09-01 10:43:46 -0700

    index page of admin working and create new user and assign role by admin in progress

--- MyStore/Controllers/AdminController.cs
+++ MyStore/Controllers/AdminController.cs
@@ -1,12 +1,86 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyStore.Areas.Identity.Data;
+using MyStore.Models;
+using System.Runtime.Intrinsics.Arm;
 
 namespace MyStore.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        //_useridentity = new UserManager
+         private readonly UserManager<MyStoreUser> _userManager;
+
+        public AdminController(UserManager<MyStoreUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            List<AdminViewModel> viewmodelist = new List<AdminViewModel>();
+            foreach (var user in users) {
+
+                var admmodel = new AdminViewModel
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Email = user.Email,
+                    Password = user.PasswordHash
+                };
+                viewmodelist.Add(admmodel); 
+            }
+           
+
+            return View(viewmodelist);
+        }
+
+        public IActionResult Create()
+        {
+            //same code as done below
+            //var rolesViewModel = new AdminViewModel();
+            //rolesViewModel.Roles = new List<string> { "Manager", "Admin", "Member" };
+
+
+            var rolesViewModel = new Models.AdminViewModel
+            {
+                Roles = new List<string> { "Manager", "Admin", "Member" }
+            };
+
+            return View(rolesViewModel);
+
+           // return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveRole(AdminViewModel user) {
+
+            if (ModelState.IsValid)
+            {
+                if (_userManager.FindByEmailAsync(user.Email) ==null)
+                {
+                   // var selectedrole = user.SelectedRole;
+                    var selectedrole = new List<string> {user.SelectedRole };
+                    var newuser = new MyStoreUser();
+                    newuser.Email = user.Email;
+                    newuser.UserName = user.Email;
+                    newuser.FirstName = user.FirstName;
+                    newuser.LastName = user.LastName;
+                    
+                    //creating new user 
+                   await _userManager.CreateAsync(newuser, user.Password);
+                    //assign role to newely created user
+                    await _userManager.AddToRolesAsync(newuser, selectedrole);
+
+                    return  RedirectToAction("Index");
+                }
+
+
+                
+            }
+            return RedirectToAction("Index");
         }
     }
 }
--- MyStore/Models/AdminViewModel.cs
+++ MyStore/Models/AdminViewModel.cs
@@ -3,10 +3,13 @@
     public class AdminViewModel
     {
         public int Id { get; set; }  // You can use an ID field if needed
-        public string Username { get; set; }
+       
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public List<string> Roles { get; set; } = new List<string>();
+        public string SelectedRole { get; set; }
 
     }
 }
